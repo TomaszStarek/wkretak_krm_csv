689f17d825be95b465654ff0461552485e9f2647
TomaszStarek <33632173+TomaszStarek@users.noreply.github.com>
2020-07-22 07:43:30 +0200

    Add files via upload

---
+++ wkertakkrom - csv/WindowsFormsApp5/WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.s1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.s2 = new System.Windows.Forms.TextBox();
+            this.s3 = new System.Windows.Forms.TextBox();
+            this.s4 = new System.Windows.Forms.TextBox();
+            this.s5 = new System.Windows.Forms.TextBox();
+            this.s6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // s1
+            // 
+            this.s1.AcceptsReturn = true;
+            this.s1.AcceptsTab = true;
+            this.s1.AllowDrop = true;
+            this.s1.Location = new System.Drawing.Point(12, 81);
+            this.s1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.s1.Multiline = true;
+            this.s1.Name = "s1";
+            this.s1.ReadOnly = true;
+            this.s1.Size = new System.Drawing.Size(255, 469);
+            this.s1.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1411, 356);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 55);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Restart aplikacji";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1425, 438);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(102, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "śruba_1";
+            // 
+            // s2
+            // 
+            this.s2.AcceptsReturn = true;
+            this.s2.AcceptsTab = true;
+            this.s2.AllowDrop = true;
+            this.s2.Location = new System.Drawing.Point(273, 81);
+            this.s2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.s2.Multiline = true;
+            this.s2.Name = "s2";
+            this.s2.ReadOnly = true;
+            this.s2.Size = new System.Drawing.Size(255, 469);
+            this.s2.TabIndex = 10;
+            // 
+            // s3
+            // 
+            this.s3.AcceptsReturn = true;
+            this.s3.AcceptsTab = true;
+            this.s3.AllowDrop = true;
+            this.s3.Location = new System.Drawing.Point(534, 81);
+            this.s3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.s3.Multiline = true;
+            this.s3.Name = "s3";
+            this.s3.ReadOnly = true;
+            this.s3.Size = new System.Drawing.Size(255, 469);
+            this.s3.TabIndex = 10;
+            // 
+            // s4
+            // 
+            this.s4.AcceptsReturn = true;
+            this.s4.AcceptsTab = true;
+            this.s4.AllowDrop = true;
+            this.s4.Location = new System.Drawing.Point(795, 81);
+            this.s4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.s4.Multiline = true;
+            this.s4.Name = "s4";
+            this.s4.ReadOnly = true;
+            this.s4.Size = new System.Drawing.Size(255, 469);
+            this.s4.TabIndex = 10;
+            // 
+            // s5
+            // 
+            this.s5.AcceptsReturn = true;
+            this.s5.AcceptsTab = true;
+            this.s5.AllowDrop = true;
+            this.s5.Location = new System.Drawing.Point(1056, 81);
+            this.s5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.s5.Multiline = true;
+            this.s5.Name = "s5";
+            this.s5.ReadOnly = true;
+            this.s5.Size = new System.Drawing.Size(255, 469);
+            this.s5.TabIndex = 10;
+            // 
+            // s6
+            // 
+            this.s6.AcceptsReturn = true;
+            this.s6.AcceptsTab = true;
+            this.s6.AllowDrop = true;
+            this.s6.Location = new System.Drawing.Point(1317, 81);
+            this.s6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.s6.Multiline = true;
+            this.s6.Name = "s6";
+            this.s6.ReadOnly = true;
+            this.s6.Size = new System.Drawing.Size(249, 469);
+            this.s6.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(370, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "śruba_2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(627, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "śruba_3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(898, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "śruba_4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1147, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "śruba_5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1422, 47);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "śruba_6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1577, 572);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.s6);
+            this.Controls.Add(this.s5);
+            this.Controls.Add(this.s4);
+            this.Controls.Add(this.s3);
+            this.Controls.Add(this.s2);
+            this.Controls.Add(this.s1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "MomentOK";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox s1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox s2;
+        private System.Windows.Forms.TextBox s3;
+        private System.Windows.Forms.TextBox s4;
+        private System.Windows.Forms.TextBox s5;
+        private System.Windows.Forms.TextBox s6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ wkertakkrom - csv/WindowsFormsApp5/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using EasyModbus;
+using System.IO.Ports;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApp5
+{
+    
+
+    public partial class Form1 : Form
+    {
+        SerialPort port;
+        string lineReadIn;
+
+        
+
+        // this will prevent cross-threading between the serial port
+        // received data thread & the display of that data on the central thread
+        private delegate void preventCrossThreading(string x);
+        private preventCrossThreading accessControlFromCentralThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+            // create and open the serial port (configured to my machine)
+            // this is a Down-n-Dirty mechanism devoid of try-catch blocks and
+            // other niceties associated with polite programming
+            const string com = "COM7";
+            port = new SerialPort(com, 9600, Parity.None, 8, StopBits.One);
+
+            //   port.ErrorReceived += new SerialErrorReceivedEventHandler();
+            try
+            {
+                port.Open();
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show("Error: Port " + com + " jest zajęty");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Uart exception: " + ex);
+            }
+
+
+ 
+
+            if (port.IsOpen)
+            {
+                // set the 'invoke' delegate and attach the 'receive-data' function
+                // to the serial port 'receive' event.
+                accessControlFromCentralThread = displayTextReadIn;
+                port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+            }
+        }
+
+      
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void zerowanieRS()
+        {
+
+            // clear the RCVbox text string and write the VER command
+            s1.Text = lineReadIn = string.Empty;
+            port.Write("VER\r");
+        }
+
+        string wydruk;
+        string[] result = new string[100];
+
+        // this is called when the serial port has receive-data for us.
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs rcvdData)
+        {
+
+            while (port.BytesToRead > 0)
+            {
+                //   lineReadIn += port.ReadExisting();
+
+                lineReadIn += port.ReadExisting();
+                lineReadIn += Environment.NewLine;
+
+                // lineReadIn += "\r\n";
+                //   lineReadIn += lineReadIn;
+                Thread.Sleep(25);
+            }
+            
+            
+            
+
+            // display what we've acquired.
+
+            wydruk = lineReadIn;
+            
+            result = wydruk.Split(',');
+
+            for (int i = 0; i < 15; i++)
+            {
+                result[i] = Regex.Replace(result[i], @"\s+", string.Empty);
+                
+            }
+
+            if (result[6] == "1")
+            {
+               // Thread.Sleep(3000);
+                s1.Text = string.Empty;
+                s2.Text = string.Empty;
+                s3.Text = string.Empty;
+                s4.Text = string.Empty;
+                s5.Text = string.Empty;
+                s6.Text = string.Empty;
+
+                s1.BackColor = SystemColors.Control;
+                s2.BackColor = SystemColors.Control;
+                s3.BackColor = SystemColors.Control;
+                s4.BackColor = SystemColors.Control;
+                s5.BackColor = SystemColors.Control;
+                s6.BackColor = SystemColors.Control;
+
+            }
+
+
+            if (result[6] == "1")
+            {
+                for (int i = 0; i < 15; i++)
+                {
+                    s1.Text += result[i] + Environment.NewLine;
+                }
+
+                if (result[4] == "O")
+                    s1.BackColor = Color.Green;
+                else
+                    s1.BackColor = Color.Red;
+            }
+
+            if (result[6] == "2")
+            {
+                for (int i = 0; i < 15; i++)
+                {
+                    s2.Text += result[i] + Environment.NewLine;
+                }
+
+                if (result[4] == "O")
+                    s2.BackColor = Color.Green;
+                else
+                    s2.BackColor = Color.Red;
+            }
+
+            if (result[6] == "3")
+            {
+                for (int i = 0; i < 15; i++)
+                {
+                    s3.Text += result[i] + Environment.NewLine;
+                }
+
+                if (result[4] == "O")
+                    s3.BackColor = Color.Green;
+                else
+                    s3.BackColor = Color.Red;
+            }
+
+            if (result[6] == "4")
+            {
+                for (int i = 0; i < 15; i++)
+                {
+                    s4.Text += result[i] + Environment.NewLine;
+                }
+
+                if (result[4] == "O")
+                    s4.BackColor = Color.Green;
+                else
+                    s4.BackColor = Color.Red;
+            }
+
+            if (result[6] == "5")
+            {
+                for (int i = 0; i < 15; i++)
+                {
+                    s5.Text += result[i] + Environment.NewLine;
+                }
+
+                if (result[4] == "O")
+                    s5.BackColor = Color.Green;
+                else
+                    s5.BackColor = Color.Red;
+            }
+
+            if (result[6] == "6")
+            {
+                for (int i = 0; i < 15; i++)
+                {
+                    s6.Text += result[i] + Environment.NewLine;
+                }
+
+                if (result[4] == "O")
+                {
+                    s6.BackColor = Color.Green;
+                }
+                else
+                    s6.BackColor = Color.Red;
+            }
+
+
+
+            if (result.Length >= 14)
+                tworzeniepliku("1");
+
+            lineReadIn = string.Empty;
+
+
+        }// end function 'port_dataReceived'
+
+
+
+        // this, hopefully, will prevent cross threading.
+        private void displayTextReadIn(string ToBeDisplayed)          //wyswietlanie sygnalu na drugim texboxie
+        {
+            if (s1.InvokeRequired)
+                s1.Invoke(accessControlFromCentralThread, ToBeDisplayed);
+            else
+                s1.Text = ToBeDisplayed;
+            
+        }
+
+        private void operacje_na_danych(string ToBeDisplayed)          //wyswietlanie sygnalu na drugim texboxie
+        {
+            if (s1.InvokeRequired)
+                s1.Invoke(accessControlFromCentralThread, ToBeDisplayed);
+            else
+            {
+ //               Dane(ToBeDisplayed);
+                s1.Text = ToBeDisplayed;
+            }
+
+        }
+
+        DateTime stop;
+        //---------------------------------------------------------------------------------------------
+
+        
+
+        const int M_BRAK_POLACZENIA_Z_MES = 4;
+
+
+
+
+        public int Test1(string SerialTxt)
+        {
+            using (MESwebservice.BoardsSoapClient wsMES = new MESwebservice.BoardsSoapClient("BoardsSoap"))
+            {
+                DataSet Result;
+                try
+                {
+                    Result = wsMES.GetBoardHistoryDS(@"itron", SerialTxt);
+                }
+                catch
+                {
+                    return M_BRAK_POLACZENIA_Z_MES;
+                }
+
+                
+            }
+                return 1;
+        }
+
+
+
+
+        private int sprawdzeniekrok(string sn)
+        {
+            int Result;
+
+            Result = Test1(sn); //przykladowy numer seryjny 9100000668
+            switch (Result)
+            {
+                case M_BRAK_POLACZENIA_Z_MES:
+                   // MessageBox.Show("Brak połączenia z MES.", "Info", MessageBoxButtons.OK);
+                    break;
+
+                default:
+                   // MessageBox.Show("Wszystko jest OK", "Info", MessageBoxButtons.OK);                   
+                    return 1;
+            }
+            MessageBox.Show("Brak połączenia z MES.", "Info", MessageBoxButtons.OK);
+            return 0;
+        }
+
+
+        private void tworzeniepliku(string sn)
+        {
+            string sciezka = ("C:/tars/");      //definiowanieścieżki do której zapisywane logi
+            stop = DateTime.Now;
+            if (Directory.Exists(sciezka))       //sprawdzanie czy sciezka istnieje
+            {
+                ;
+            }
+            else
+                System.IO.Directory.CreateDirectory(sciezka); //jeśli nie to ją tworzy
+
+            result[0] = Regex.Replace(result[0], @"\s+", string.Empty);
+            result[6] = Regex.Replace(result[6], @"\s+", string.Empty);
+            using (StreamWriter sw = new StreamWriter("C:/tars/" + result[0] + "-" + result[6] + "-" + "(" + stop.Day + "-" + stop.Month + "-" + stop.Year  + " "+ stop.Hour + "-" + stop.Minute + "-" + stop.Second + ")" + ".Tars"))
+            {
+                
+
+                sw.WriteLine("S{0}", result[0]);
+                sw.WriteLine("CITRON");
+                sw.WriteLine("NPLKWIM0P19B3M01_" + result[6]);
+                sw.WriteLine("PQC_SCREW" + result[6]);
+                sw.WriteLine("Ooperator");
+
+                result[1] = Regex.Replace(result[1], @"\s+", string.Empty);
+                sw.WriteLine("[" + result[1].Remove(10) + " " + result[1].Remove(0, 10));
+                //  sw.WriteLine("]" + stop.Year + "-" + stop.Month + "-" + stop.Day + " " + stop.Hour + ":" + stop.Minute + ":" + stop.Second);
+                sw.WriteLine("]" + stop.ToString("yyyy-MM-dd HH:mm:ss"));
+                if (result[4] == "O")
+                    sw.WriteLine("TP");
+                else
+                    sw.WriteLine("TF");
+
+                sw.WriteLine("MResult_" + result[6]);
+                sw.WriteLine("d" + result[5].Trim());
+                sw.WriteLine("MCount_" + result[6]);
+                sw.WriteLine("d" + result[6].Trim());
+                sw.WriteLine("MBatch_" + result[6]);
+                sw.WriteLine("d" + result[7].Trim());
+                sw.WriteLine("MTorque Actual_" + result[6]);
+                sw.WriteLine("d" + result[8]);
+                result[9] = Regex.Replace(result[9], @"\s+", string.Empty);
+                sw.WriteLine("MTorque Min_" + result[6]);
+                sw.WriteLine("d" + result[9]);
+                sw.WriteLine("MTorque Max_" + result[6]);
+                sw.WriteLine("d" + (result[10])  );
+                sw.WriteLine("MTorque Units_" + result[6]);
+                sw.WriteLine("d" + result[11]);
+                sw.WriteLine("MAngle_" + result[6]);
+                sw.WriteLine("d" + result[12]);
+                sw.WriteLine("MAngle Min_" + result[6]);
+                sw.WriteLine("d" + result[13] );
+                sw.WriteLine("MAngle Max_" + result[6]);
+                sw.WriteLine("d" + result[14]);
+
+
+                //for (int i = 0; i > 15; i++)
+                //    result[i] = string.Empty;
+
+            }
+
+
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            //1
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "0:" + result[0] + "\n" + "1:" + result[1] + "\n" + "2:" + result[2] + "\n" + "3:" + result[3] + "\n" + "4:" + result[4] + "\n" + "5:" + result[5] + "\n" + "6:" + result[6] + "\n" + "7:" + result[7] + "\n";
+        }
+
+    }
+}
